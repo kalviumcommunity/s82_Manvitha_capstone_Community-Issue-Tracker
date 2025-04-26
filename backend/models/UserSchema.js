f6f9fd3a52b080411cf400c1950d3d178d80f738
manvitha <jsmr486@gmail.com>
2025-04-26 17:36:58 +0530

    done

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -1,27 +1,15 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const userSchema=new mongoose.Schema({
-    phoneNumber:{
-        type:String,
-        required:true,
-        unique:true,
-        match: /^[0-9]{10}$/
-    },
-    password:{
-        type:String,
-        required:true
-
-    },
-    role:{
-        type:String,
-        enum:['resident','president'],
-        default:'resident',
-        required:true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-      }
-})
+const userSchema = new mongoose.Schema({
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
 
 module.exports = mongoose.model('User', userSchema);
