d717d1876ca11d768fafc949e84164fbdf66a1b3
manvitha <jsmr486@gmail.com>
2025-05-06 14:11:00 +0530

    done

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  phoneNumber: {
+  mail: {
     type: String,
     required: true,
     unique: true,
-    match: /^[0-9]{10}$/
+    lowercase: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
   },
   password: {
     type: String,
