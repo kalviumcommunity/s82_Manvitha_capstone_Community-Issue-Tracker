518e612ea7db3cc1dafbbbe534b085d9770695f9
manvitha <jsmr486@gmail.com>
2025-05-14 14:41:51 +0530

    done

---
+++ backend/models/IssueSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const issueSchema = new mongoose.Schema({
+  title: {
+    type:String,
+    required:true
+  },
+  description:{
+    type:String
+  },
+  category:{
+    type:String,
+    required:true
+  },
+  status: {
+    type: String,
+    enum: ['open', 'in progress', 'resolved'],
+    default: 'open'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Issue', issueSchema);
